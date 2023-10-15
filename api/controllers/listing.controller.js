5474c98159960780d724299a2ee7d425c78c2456
Shivam Modi <modishivam037@gmail.com>
2023-10-15 21:44:14 +0530

    created update API route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing=async(req,res,next)=>{
 
@@ -28,4 +29,24 @@ export const deleteListing=async(req,res,next)=>{
         next(error);
         
     }
+}
+
+export const updateListing=async(req,res,next)=>{
+    const listing=await Listing.findById(req.params.id);
+    if(!listing){
+        return next(errorHandler(404,'Listing not Found'));
+
+    }
+    if(req.user.id !== listing.userRef){
+        return next(errorHandler(401,'User listing not Found'));
+    }
+    try {
+        await Listing.findByIdAndUpdate(req.params.id, req.body, {new:true});
+        res.status(201).json(updateListing); 
+        
+    } catch (error) {
+        next(error);
+    }
+
+
 }
