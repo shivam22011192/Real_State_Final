03e1a8de58f400ab60f8f09de5b9fa8eed902cb0
Shivam Modi <modishivam037@gmail.com>
2023-10-15 21:05:20 +0530

    completed User Listing Delete func

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -10,3 +10,22 @@ export const createListing=async(req,res,next)=>{
         next(error);
     }
 }
+
+export const deleteListing=async(req,res,next)=>{
+
+    const listing=await Listing.findById(req.params.id);
+    if(!listing){
+        return next(errorHandler(404,'Listing not Found'));
+
+    }
+    if(req.user.id !== listing.userRef){
+        return next(errorHandler(401,'User listing not Found'));
+    }
+    try {
+        await Listing.findByIdAndDelete(req.params.id);
+        res.status(201).json('listing has been deleted')
+    } catch (error) {
+        next(error);
+        
+    }
+}
